712839499bea8068dea4c9b3d9e7013b4afa8b9c
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-07-26 14:08:14 -0300

    Treinamentos do pyautogui

---
+++ intensivão de Python/AutoguiTreinamentos.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+
+
+pyautogui.PAUSE = 1
+
+
+pyautogui.press("win")
+pyautogui.write("bloco de notas")
+pyautogui.press("enter")
+time.sleep(1)
+pyautogui.write(
+    "Ola esse é um exemplo de como entrar no Bloco de Notas e Digitar algo sem que você mexa no seu computador")
