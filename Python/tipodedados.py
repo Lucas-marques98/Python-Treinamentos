41b9b4232fd9e22890f2c42e3f907456f3374e09
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-08-07 21:56:37 -0300

    Ex tipo de dados

---
+++ Python/tipodedados.py
@@ -0,0 +1,25 @@
+x=1 #int / valor inteiro
+x= "Lucas Marques" #string
+x= 30.5 #float
+y= False #bool / Bolean
+n1=5;n2=4;y=complex(n1,n2) #numero complexo. Parte real é o n1, parte imaginária é o n2.
+
+l=['carro','moto','barco'] #list / Array / posso colocar tipo de dados diferente dentro de uma lista.
+
+l[0]='Onibus' #alterando o valor do indice 0.
+s=range(0,10,1) # cria uma lista de 0 a 10 de 1 em 1.
+print(s)
+
+print(l[0]) #imprimindo um valor especifico da lista, coloco o indice do valor entre cochetes [].
+
+m=('dinheiro','viagem','sucesso') #tupla / ela é parecido com um Array mas eu não posso substituir um elemento.
+
+print(m)
+
+
+# print("o Valor de ",x) #mostra o valor de x
+# print("O tipo de x ",type(x)) #imprime o tipo da váriavel
+
+# print("A parte real e",y.real) #mostra a parte Real
+# print("A parte imaginaria e",y.imag) #mostra a parte imaginária
+
