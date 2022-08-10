f3b55a3c0f37de106c4dc906028bccf18dfab9b4
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-08-10 20:46:24 -0300

    Ex Operações Casting

---
+++ Python/OpCastiing.py
@@ -0,0 +1,18 @@
+num_i = 10  # tipo inteiro
+num_f = 5.2  # tipo float
+num_c = 1j  # tipo complexo
+
+x = int(num_f)  # convertendo numeros float em inteiro
+l = float(num_i)  # convertendo número inteiro em float
+
+# posso fazer desse jeito, ou simplesmente usar virgulas invés do sinal de +
+print("valor: " + str(x) + " - Tipo" + str(type(x)))
+# concatenando um numero com string
+
+print("valor: " + str(l) + " - Tipo" + str(type(l)))
+
+
+#       OPERAÇÕES DE CASTING
+# str para converter um num inteiro em uma String
+# int para converter uma string em um num inteiro
+# float para converter um numero inteiro em flutuante
