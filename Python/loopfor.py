25bc274eea017d14c233a37aca50c53f6532e187
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-08-17 01:49:11 -0300

    Ex For in

---
+++ Python/loopfor.py
@@ -0,0 +1,8 @@
+nomes = ['Lucas' , 'Leticia' , 'milena'] #usando listas de strings
+for nome in nomes: #condição do for
+  print("Ola", nome)
+
+  l = [1, 2 , 3, 4] #usando listas de números
+
+  for n in l:
+    print(3 + n) #somando o valor de 3 em 3
