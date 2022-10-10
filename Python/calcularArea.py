0890369a09eae72f4fbd59f20d6013f700079551
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-10-09 21:24:42 -0300

    New Exercicio

---
+++ Python/calcularArea.py
@@ -0,0 +1,24 @@
+a : int
+b : int
+resultado : int
+
+a = 5
+b = 2
+resultado = a / b #Divisão normal
+
+print(resultado)
+
+# o python ele ignora mesmo quando a gente declara á variavel como int 
+# e gera contas que retornam números float
+
+# para eu fazer divisão inteira eu devo usar duas barras // para que ele me retorne uma divisão inteira
+
+a : int
+b : int
+resultado : int
+
+a = 5
+b = 2
+resultado = a // b #DIVISÃO INTEIRA
+
+print(resultado)
