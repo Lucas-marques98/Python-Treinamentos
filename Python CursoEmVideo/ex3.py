fc8cd5e48151e6707db94b3c8889a5f8fd315d5a
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-10-21 22:38:30 -0300

    New commit

---
+++ Python CursoEmVideo/ex3.py
@@ -0,0 +1,8 @@
+#Faça um programa em que o usuário digite dois valores e faz a soma e mostra o resultado na tela.
+
+v1 = int(input('Por favor digite o valor 1:'))
+v2 = int(input('Por favor digite o valor 2:'))
+
+somar = v1 + v2
+
+print(f'A soma entre {v1} + {v2} é = {somar}')
---
+++ Python CursoEmVideo/ex4.py
@@ -0,0 +1,17 @@
+#Dissecando uma váriavel
+
+#Faça um programa que leia algo pelo teclado e mostra na tela o seu tipo primitivo e todas informações possiveis sobre ele.
+
+algo = input('Por favor digite algo:')
+
+print('O tipo primitivo desse valor é ', type(algo))
+print('Só tem espaços? ', algo.isspace())
+print('É um número?', algo.isnumeric())
+print('É alfabético?', algo.isalpha())
+print('É alfanumérico?', algo.isalnum())
+print('Está em maiúsculas?', algo.isupper())
+print('Está em minúsculas?', algo.islower())
+print('Está em capitalizada?', algo.istitle())
+
+#Toda vez que estivermos trabalhando com strings, eles tem seus métodos.
+#Métodos são todos que tem parenteses no fim.
