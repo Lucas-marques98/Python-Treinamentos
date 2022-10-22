d6c7fd0f03c51ce773ab3323dfd8a49d223807bc
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-10-21 22:12:35 -0300

    new commit

---
+++ Python CursoEmVideo/ex1.py
@@ -0,0 +1,5 @@
+#Crie um programa que escreva olá mundo na tela
+
+msg = 'Ola mundo, Estou fazendo os exercicios do curso em video'
+
+print(msg)
---
+++ Python CursoEmVideo/ex2.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia o nome de uma pessoa e mostra a mensagem de boas vindas.
+
+nome = input('Ola, por favor digite seu nome:')
+
+print(f'É um prazer te conhecer {nome} sejá bem vindo(a)!!!')
